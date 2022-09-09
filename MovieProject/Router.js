f5626bc1d85ab780d5d3bba26896fa5e14d9d1c9
barissemerci <barissemerci3@gmail.com>
2022-09-10 01:51:43 +0300

    ESLint and Prettier changes

--- MovieProject/Router.js
+++ MovieProject/Router.js
@@ -1,31 +1,16 @@
-import React,{useRef} from 'react';
+import React, {useRef} from 'react';
 import MainContainer from './src/navigation/MainContainer';
-import FlashMessage from "react-native-flash-message";
+import FlashMessage from 'react-native-flash-message';
 
 function App() {
   const ref = useRef();
   return (
-  
-  <>
-   
-   <MainContainer />
-  
-  <FlashMessage ref={ref} />
-  
-  </>
- 
-  
-  
-  )
-  
-  
-  
-  
-  ;
+    <>
+      <MainContainer />
 
-
-
-
+      <FlashMessage ref={ref} />
+    </>
+  );
 }
 
 export default App;
--- MovieProject/index.js
+++ MovieProject/index.js
@@ -2,9 +2,8 @@
  * @format
  */
 
- import {AppRegistry} from 'react-native';
- import Router from './Router';
- import {name as appName} from './app.json';
- 
- AppRegistry.registerComponent(appName, () => Router);
- 
+import {AppRegistry} from 'react-native';
+import Router from './Router';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Router);
